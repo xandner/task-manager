0c36ccc361ad89e039fe2129a3db647153a940f5
xander <k.ehsan75@gmail.com>
2025-03-19 10:53:01 +0330

    feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //? swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Tasks API Docs')
+    .setDescription('API documents for task manager app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
